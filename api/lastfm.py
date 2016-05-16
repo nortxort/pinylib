c13e20aafbda7d665124c03a1a0109343a4203c6
nortxort <nortxort@users.noreply.github.com>
2016-05-16 15:18:02 +0200

    Update lastfm.py

--- api/lastfm.py
+++ api/lastfm.py
@@ -1,5 +1,9 @@
+import logging
 import web_request
 import youtube
+
+
+log = logging.getLogger(__name__)
 
 
 def get_lastfm_chart(chart_items=5):
@@ -10,6 +14,7 @@ def get_lastfm_chart(chart_items=5):
     """
     url = 'http://lastfm-ajax-vip1.phx1.cbsig.net/kerve/charts?nr=%s&type=track&format=json' % chart_items
     lastfm = web_request.get_request(url, json=True)
+    log.debug(lastfm)
     if lastfm is not None:
         if 'results' in lastfm['content']:
             if 'track' in lastfm['content']['results']:
@@ -18,6 +23,7 @@ def get_lastfm_chart(chart_items=5):
                     for track in lastfm['content']['results']['track']:
                         search_str = '%s - %s' % (track['artist'], track['name'])
                         yt = youtube.youtube_search(search_str)
+                        log.info(yt)
                         if yt is not None:
                             yt_tracks.append(yt)
                     return yt_tracks
@@ -35,6 +41,7 @@ def search_lastfm_by_tag(search_str, by_id=True, max_tunes=40):
     url = 'http://lastfm-ajax-vip1.phx1.cbsig.net/kerve/charts?nr=%s&type=track&f=tag:%s&format=json' % \
           (max_tunes, search_str)
     lastfm = web_request.get_request(url, json=True)
+    log.debug(lastfm)
     if lastfm is not None:
         if 'track' in lastfm['content']['results']:
             if len(lastfm['content']['results']['track']) is not 0:
@@ -45,16 +52,19 @@ def search_lastfm_by_tag(search_str, by_id=True, max_tunes=40):
                         if 'data-youtube-id' in track['playlink']:
                             youtube_id = track['playlink']['data-youtube-id']
                             yt = youtube.youtube_time(youtube_id)
+                            log.debug(yt)
                             if yt is not None:
                                 yt_tracks.append(yt)
                         else:
                             if not by_id:
                                 yt = youtube.youtube_search(search_str)
+                                log.debug('search by search string: %s result: %s' % (search_str, yt))
                                 if yt is not None:
                                     yt_tracks.append(yt)
                     else:
                         if not by_id:
                             yt = youtube.youtube_search(search_str)
+                            log.debug('search by search string: %s result: %s' % (search_str, yt))
                             if yt is not None:
                                 yt_tracks.append(yt)
                 return yt_tracks
@@ -70,6 +80,7 @@ def lastfm_listening_now(max_tunes, by_id=True):
     """
     url = 'http://lastfm-ajax-vip1.phx1.cbsig.net/kerve/listeningnow?limit=%s&format=json' % max_tunes
     lastfm = web_request.get_request(url, json=True)
+    log.debug(lastfm)
     if lastfm is not None:
         if len(lastfm['content']['Users']) is not 0:
             yt_tracks = []
@@ -78,6 +89,7 @@ def lastfm_listening_now(max_tunes, by_id=True):
                     if 'data-youtube-id' in user['playlink']:
                         youtube_id = user['playlink']['data-youtube-id']
                         yt = youtube.youtube_time(youtube_id)
+                        log.debug(yt)
                         if yt is not None:
                             yt_tracks.append(yt)
                 else:
@@ -85,6 +97,7 @@ def lastfm_listening_now(max_tunes, by_id=True):
                         search_str = '%s - %s' % (user['Track']['Artist'], user['Track']['Name'])
                         if not by_id:
                             yt = youtube.youtube_search(search_str)
+                            log.debug('search by search string: %s result: %s' % (search_str, yt))
                             if yt is not None:
                                 yt_tracks.append(yt)
             return yt_tracks
