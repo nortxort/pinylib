c2dbdbc36fa8bf95f198c00c634f3c56559cf885
nortxort <nortxort@users.noreply.github.com>
2016-08-12 21:37:15 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""
+ pinylib (originally called tinylib) provides an easy way to interface with tinychat chat rooms.
+
+ It contains classes/methods and functions to establish a connection
+ to tinychat's RTMP server. The idea was to make a library/module that would serve
+ as a building block for developers, wanting to make a helper and/or entertainment bot.
+ """
