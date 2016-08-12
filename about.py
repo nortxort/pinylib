9bdd5b998503f853c59b39d9ed8c6dd8cc9ec607
nortxort <nortxort@users.noreply.github.com>
2016-08-12 21:37:50 +0200

    Create about.py

---
+++ about.py
@@ -0,0 +1,6 @@
+__author__ = 'nortxort'
+__copyright__ = 'Copyright 2016, nortxort'
+__credits__ = ['MegaLoler', 'notnola', 'GoelBiju', 'prekageo', 'Anorov', 'hydralabs']
+__license__ = 'MIT'
+__version__ = '5.0.0'
+__maintainer__ = 'nortxort'
