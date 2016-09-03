a091e090aff39f546d972574760fa2f7a7a60c28
nortxort <nortxort@users.noreply.github.com>
2016-09-03 20:07:43 +0200

    Update about.py

--- about.py
+++ about.py
@@ -2,4 +2,4 @@ __author__ = 'nortxort'
 __copyright__ = 'Copyright 2016, nortxort'
 __credits__ = ['MegaLoler', 'notnola', 'GoelBiju', 'prekageo', 'Anorov', 'hydralabs']
 __license__ = 'MIT'
-__version__ = '5.0.0'
+__version__ = '5.0.1'
