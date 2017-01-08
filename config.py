4bbd9d61e28e415bc69a59ce2b5dac45ed1bdbaf
nortxort <nortxort@users.noreply.github.com>
2017-01-08 19:47:09 +0100

    version 6.0.0

---
+++ config.py
@@ -0,0 +1,28 @@
+# This file holds pinylib's configuration.
+# Use a prefix, if you plan on adding your own.
+# Settings info https://github.com/nortxort/pinylib/wiki/Settings
+
+# The swf version that tinychat is currently using.
+SWF_VERSION = '0675'
+# Log chat messages and events.
+CHAT_LOGGING = True
+# Show additional info/errors in console.
+DEBUG_MODE = True
+# Log debug info to file.
+DEBUG_TO_FILE = True
+# Use colors for the console.
+CONSOLE_COLORS = True
+# Enable auto job (recommended)
+ENABLE_AUTO_JOB = True
+# Time format.
+USE_24HOUR = True
+# Reset the run time after a reconnect.
+RESET_INIT_TIME = False
+# Reconnect delay in seconds.
+RECONNECT_DELAY = 60
+# Auto job interval in seconds.
+AUTO_JOB_INTERVAL = 600
+# The name of pinylib's debug log file.
+DEBUG_FILE_NAME = 'pinylib_debug.log'
+# The path to the config folder.
+CONFIG_PATH = 'rooms/'
