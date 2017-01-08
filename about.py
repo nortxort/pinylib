2d40355146df52dcce96855b91917533fa67f569
nortxort <nortxort@users.noreply.github.com>
2017-01-08 19:35:02 +0100

    Delete about.py

--- about.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'nortxort'
-__copyright__ = 'Copyright 2016, nortxort'
-__credits__ = ['MegaLoler', 'notnola', 'GoelBiju', 'prekageo', 'Anorov', 'hydralabs']
-__license__ = 'MIT'
-__version__ = '5.0.2'
