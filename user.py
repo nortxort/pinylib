f638f9d372f23e7343a27b3ce1b605a3bbbb4a68
nortxort <nortxort@users.noreply.github.com>
2017-01-18 17:23:09 +0100

    Update user.py

--- user.py
+++ user.py
@@ -37,7 +37,7 @@ class Users:
     def __init__(self):
         # Create a dictionary to store each user key value in.
         self._users = dict()
-    
+
     @property
     def all(self):
         """
